e1372f49d970a61b4039dc0d268287b7ee11e897
Evelyn Ita <evelyn.ita@wemabank.com>
2022-11-10 14:59:57 +0100

    Made update to contact page

--- src/component/commons/Form.jsx
+++ src/component/commons/Form.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { BsArrowRightShort } from 'react-icons/bs'
 import Location from '../../images/location.png'
 import Mail from '../../images/mail.png'
@@ -6,49 +6,45 @@ import Phone from '../../images/phone.png'
 import TextInput from '../commons/TextInput';
 
 const Form = () => {
-  return (
-      <section className="bg-secondary pt-24 pb-44">
-          <div className="flex lg:flex-row flex-col justify-center gap-10 max-w-6xl m-auto p-5">
-              <div className="text-white lg:w-1/3 w-full">
-                  <h3 className="font-semibold text-lg mb-10">Contact Us</h3>
-                  <p className="flex items-center gap-4 mb-10">
-                      <img src={Location} alt="" className="h-7 w-7" />
-                      127, Lagos Abeokuta Expressway, Ade-Alu Bus-Stop, Iyana Ipaja
-                  </p>
-                  <p className="flex items-center gap-4 mb-10">
-                      <img src={Mail} alt="" className="h-7 w-7" />
-                      @morolayofoundation.org
-                  </p>
-                  <p className="flex items-center gap-4">
-                      <img src={Phone} alt="" className="h-7 w-7" />
-                      08134566733
-                  </p>
-              </div>
-              <div className="bg-white lg:w-1/2 w-full lg:p-16 p-7">
-                  <TextInput
-                      className=" mb-5 mt-10"
-                      placeholder="Name"
-                  />
-                  <TextInput
-                      className=" mb-5"
-                      placeholder="Email"
-                  />
-                  <TextInput
-                      className=" mb-5"
-                      placeholder="Phone"
-                  />
-                  <TextInput
-                      className=" mb-5"
-                      placeholder="Message"
-                  />
-                  <button type="submit" className="flex items-center gap-3 bg-black text-white p-2 mt-10 mb-16">
-                      Submit <BsArrowRightShort />
-                  </button>
+    const NameRef = useRef(null);
+    const EmailRef = useRef(null);
+    const PhoneNumberRef = useRef(null);
+    const MessageRef = useRef(null);
 
-              </div>
-          </div>
-      </section>
-  )
+    return (
+
+        <form action="https://sheetdb.io/api/v1/s97k1f7xlof2p" method="post"
+            id="sheetdb-form" className="bg-secondary pt-24 pb-44">
+            <div className="flex lg:flex-row flex-col justify-center gap-10 max-w-6xl m-auto p-5">
+                <div className="text-white lg:w-1/3 w-full">
+                    <h3 className="font-semibold text-lg mb-10">Contact Us</h3>
+                    <p className="flex items-center gap-4 mb-10">
+                        <img src={Location} alt="" className="h-7 w-7" />
+                        127, Lagos Abeokuta Expressway, Ade-Alu Bus-Stop, Iyana Ipaja
+                    </p>
+                    <p className="flex items-center gap-4 mb-10">
+                        <img src={Mail} alt="" className="h-7 w-7" />
+                        @morolayofoundation.org
+                    </p>
+                    <p className="flex items-center gap-4">
+                        <img src={Phone} alt="" className="h-7 w-7" />
+                        08134566733
+                    </p>
+                </div>
+                <div className="bg-white lg:w-1/2 w-full lg:p-16 p-7">
+                    <input type="text" ref={NameRef} id="input" placeholder="Name" name="data[Name]" required />
+                    <input type="email" ref={EmailRef} id="input" placeholder="Email" name="data[Email]" required />
+                    <input type="text" ref={PhoneNumberRef} id="input" placeholder="Phone Number" name="data[Phone]" required />
+                    <textarea className="w-full outline-none border-b border-black mt-5 " ref={MessageRef} name="data[message]" id="" cols="20" rows="10" placeholder="Message" required></textarea>
+                    <button type="submit" className="flex items-center gap-3 bg-black text-white p-2 mt-10 mb-16">
+                        Submit <BsArrowRightShort />
+                    </button>
+
+                </div>
+            </div>
+        </form>
+
+    )
 }
 
 export default Form
--- src/utilities/general.js
+++ src/utilities/general.js
@@ -8,3 +8,21 @@ export const toggleScroll = () => {
         document.body.classList.remove('no-scroll');
     }
 };
+
+export const form = document.getElementById('sheetdb-form');
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    fetch(form.action, {
+        method: "POST",
+        body: new FormData(document.getElementById("sheetdb-form")),
+    }).then((response) => {
+        console.log(response.json())
+    }
+
+        //console.log(response.json);
+    ).then((html) => {
+        // you can put any JS code here
+        window.open('page2.html', '_blank');
+
+    });
+});
